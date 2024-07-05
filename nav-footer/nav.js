a935e41d93e5fb14ef0c1d29f57567a9b63216c2
zt8media <zt8media@gmail.com>
2024-07-05 10:12:08 -0400

    update

--- nav-footer/nav.js
+++ nav-footer/nav.js
@@ -91,17 +91,21 @@ document.addEventListener("DOMContentLoaded", function() {
         let passed = true;
         username.classList.remove('red-border');
         password.classList.remove('red-border');
-        if (username.value === '' || !/^[a-zA-Z0-9]{4,}$/.test(username.value)) {
+        if (username === '' || !/^[a-zA-Z0-9]{4,}$/.test(username.value) || username.value.length > 20){
             username.classList.add('red-border');
             passed = false;
         }
-        if (password.value === '' || !/^[a-zA-Z0-9]{4,}$/.test(password.value)) {
+        if (password === '' || !/^[a-zA-Z0-9]{4,}$/.test(password.value || password.value.length > 20)){
             password.classList.add('red-border');
             passed = false;
         }
-        return passed;
+        if (passed) {
+            username.classList.remove('red-border');
+            password.classList.remove('red-border');
+            return true;
+        }
+        return false;
     }
-
     function signValidation() {
         const email = signUp_email;
         const username = signUp_username;
@@ -110,26 +114,50 @@ document.addEventListener("DOMContentLoaded", function() {
         email.classList.remove('red-border');
         username.classList.remove('red-border');
         password.classList.remove('red-border');
-        if (email.value === '' || !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+        if (email === '' || !/^\w+([.-]?\w+)@\w+([.-]?\w+)(\.\w{2,3})+$/.test(email.value)){
             email.classList.add('red-border');
             passed = false;
         }
-        if (username.value === '' || !/^[a-zA-Z0-9]{4,}$/.test(username.value)) {
+        if (email.value.length > 255) {
+            email.classList.add('red-border');
+            passed = false;
+            alert('Email must be less than 255 characters');
+        }
+        if (username === '' || !/^[a-zA-Z0-9]{4,}$/.test(username.value)){
             username.classList.add('red-border');
             passed = false;
+        } else if (username.value.length > 20 && !password.value.length > 20) {
+            username.classList.add('red-border');
+            passed = false;
+            alert('Username must be less than 20 characters');
         }
-        if (password.value === '' || !/^[a-zA-Z0-9]{4,}$/.test(password.value)) {
+        if (password === '' || !/^[a-zA-Z0-9]{4,}$/.test(password.value)){
             password.classList.add('red-border');
             passed = false;
+        } else if (password.value.length > 20 && !username.value.length > 20) {
+            password.classList.add('red-border');
+            passed = false;
+            alert('Password must be less than 20 characters');
         }
-        return passed;
+        if (username.value.length > 20 && password.value.length > 20) {
+            username.classList.add('red-border');
+            password.classList.add('red-border');
+            passed = false;
+            alert('Username and password must be less than 20 characters');
+        }
+        if (passed) {
+            email.classList.remove('red-border');
+            username.classList.remove('red-border');
+            password.classList.remove('red-border');
+            return true;
+        }
+        return false;
     }
 
     // Form submission event listeners
     document.getElementById('login-submit').addEventListener('click', (event) => {
-        event.preventDefault();
         if (!loginValidation()) {
-            return;
+            return
         }
         const options = {
             method: 'POST',
@@ -141,21 +169,33 @@ document.addEventListener("DOMContentLoaded", function() {
                 password: login_password.value
             })
         };
-        fetch('/login', options)
-            .then(response => response.json())
+        fetch(`/login`, options)
+            .then(response => {
+                if (response.status === 401) {
+                    alert('Invalid username or password');
+                    return;
+                }
+                return response.json();
+            })
             .then(data => {
+                if (!data) {
+                    return;
+                }
                 if (data.message) {
                     alert(data.message);
                     return;
                 }
-                document.cookie = `token=${data.token}; path=/;`;
-                window.location = data.admin ? '/admin' : '/home';
+                document.cookie = `token=${data.token}; path=/`;
+                if (data.admin) {
+                    window.location = '/admin';
+                }
+                else {
+                    window.location = '/home';
+                }
             })
             .catch(error => console.error(error));
     });
-
     document.getElementById('signUp-submit').addEventListener('click', (event) => {
-        event.preventDefault();
         if (!signValidation()) {
             return;
         }
@@ -170,9 +210,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 password: signUp_password.value
             })
         };
-        fetch('/signup', options)
-            .then(response => response.json())
-            .then(data => {
+        fetch(`/signup`, options)
+            .then(response => {
                 if (response.status === 409) {
                     alert('Username already exists');
                     return;
@@ -181,20 +220,20 @@ document.addEventListener("DOMContentLoaded", function() {
                     alert('Internal server error');
                     return;
                 }
-                window.location = data.admin ? '/admin' : '/home';
+                return response.json();
+            })
+            .then(data => {
+                if (!data) {
+                    return;
+                }
+                if (data.admin) {
+                    window.location = '/admin';
+                }
+                else {
+                    window.location = '/home';
+                }
             })
             .catch(error => console.error(error));
-    });
-
-    // Form switch event listeners
-    document.getElementById('signUp').addEventListener('click', () => {
-        loginForm.classList.add('hide');
-        signUpForm.classList.remove('hide');
-    });
-
-    document.getElementById('login').addEventListener('click', () => {
-        signUpForm.classList.add('hide');
-        loginForm.classList.remove('hide');
     });
 });
 
